26fb012169e1bfaa273a07fb39d87568a1c81fa5
Eric Vicenti <ericvicenti@gmail.com>
2018-02-01 09:57:45 -0800

    New createNavigator and View API
    See the RFC here:
    https://github.com/react-navigation/rfcs/blob/master/text/0002-navigator-view-api.md

--- flow/react-navigation.js
+++ flow/react-navigation.js
@@ -71,22 +71,8 @@ declare module 'react-navigation' {
     key?: string,
   |};
 
-  declare type DeprecatedNavigationNavigateAction = {|
-    type: 'Navigate',
-    routeName: string,
-    params?: NavigationParams,
-
-    // The action to run inside the sub-router
-    action?: NavigationNavigateAction | DeprecatedNavigationNavigateAction,
-  |};
-
   declare export type NavigationBackAction = {|
     type: 'Navigation/BACK',
-    key?: ?string,
-  |};
-
-  declare type DeprecatedNavigationBackAction = {|
-    type: 'Back',
     key?: ?string,
   |};
 
@@ -100,23 +86,8 @@ declare module 'react-navigation' {
     params: NavigationParams,
   |};
 
-  declare type DeprecatedNavigationSetParamsAction = {|
-    type: 'SetParams',
-
-    // The key of the route where the params should be set
-    key: string,
-
-    // The new params to merge into the existing route params
-    params: NavigationParams,
-  |};
-
   declare export type NavigationInitAction = {|
     type: 'Navigation/INIT',
-    params?: NavigationParams,
-  |};
-
-  declare type DeprecatedNavigationInitAction = {|
-    type: 'Init',
     params?: NavigationParams,
   |};
 
@@ -127,22 +98,8 @@ declare module 'react-navigation' {
     actions: Array<NavigationNavigateAction>,
   |};
 
-  declare type DeprecatedNavigationResetAction = {|
-    type: 'Reset',
-    index: number,
-    key?: ?string,
-    actions: Array<
-      NavigationNavigateAction | DeprecatedNavigationNavigateAction
-    >,
-  |};
-
   declare export type NavigationUriAction = {|
     type: 'Navigation/URI',
-    uri: string,
-  |};
-
-  declare type DeprecatedNavigationUriAction = {|
-    type: 'Uri',
     uri: string,
   |};
 
@@ -180,17 +137,6 @@ declare module 'react-navigation' {
     | NavigationBackAction
     | NavigationSetParamsAction
     | NavigationResetAction;
-
-  declare type DeprecatedNavigationAction =
-    | DeprecatedNavigationInitAction
-    | DeprecatedNavigationNavigateAction
-    | DeprecatedNavigationBackAction
-    | DeprecatedNavigationSetParamsAction
-    | DeprecatedNavigationResetAction;
-
-  declare export type PossiblyDeprecatedNavigationAction =
-    | NavigationAction
-    | DeprecatedNavigationAction;
 
   /**
    * NavigationState is a tree of routes for a single navigator, where each
@@ -479,7 +425,7 @@ declare module 'react-navigation' {
    */
 
   declare export type NavigationDispatch = (
-    action: PossiblyDeprecatedNavigationAction
+    action: NavigationAction
   ) => boolean;
 
   declare export type NavigationProp<S> = {
@@ -542,29 +488,6 @@ declare module 'react-navigation' {
     screenProps?: {},
     navigationOptions?: O,
   }>;
-
-  //declare export type NavigationNavigatorProps<O: {}, S: {}> =
-  //  | {}
-  //  | { navigation: NavigationScreenProp<S> }
-  //  | { screenProps: {} }
-  //  | { navigationOptions: O }
-  //  | {
-  //      navigation: NavigationScreenProp<S>,
-  //      screenProps: {},
-  //    }
-  //  | {
-  //      navigation: NavigationScreenProp<S>,
-  //      navigationOptions: O,
-  //    }
-  //  | {
-  //      screenProps: {},
-  //      navigationOptions: O,
-  //    }
-  //  | {
-  //      navigation: NavigationScreenProp<S>,
-  //      screenProps: {},
-  //      navigationOptions: O,
-  //    };
 
   /**
    * Navigation container
@@ -740,10 +663,6 @@ declare module 'react-navigation' {
     ) => NavigationState,
   };
 
-  declare export function addNavigationHelpers<S: {}>(
-    navigation: NavigationProp<S>
-  ): NavigationScreenProp<S>;
-
   declare export var NavigationActions: {
     BACK: 'Navigation/BACK',
     INIT: 'Navigation/INIT',
@@ -786,31 +705,29 @@ declare module 'react-navigation' {
       (payload: { uri: string }): NavigationUriAction,
       toString: () => string,
     },
-    mapDeprecatedActionAndWarn: (
-      action: PossiblyDeprecatedNavigationAction
-    ) => NavigationAction,
   };
 
   declare type _RouterProp<S: NavigationState, O: {}> = {
     router: NavigationRouter<S, O>,
   };
-  declare type _NavigatorCreator<
-    NavigationViewProps: {},
-    S: NavigationState,
-    O: {}
-  > = (
-    NavigationView: React$ComponentType<_RouterProp<S, O> & NavigationViewProps>
-  ) => NavigationNavigator<S, O, NavigationViewProps>;
-  declare export function createNavigator<
-    S: NavigationState,
-    O: {},
-    NavigatorConfig: {},
-    NavigationViewProps: NavigationNavigatorProps<O, S>
-  >(
+
+  declare type NavigationDescriptor = {
+    key: string,
+    state: NavigationLeafRoute | NavigationStateRoute,
+    navigation: NavigationScreenProp<*>,
+    getComponent: () => React$ComponentType<{}>,
+  };
+
+  declare type NavigationView<O, S> = React$ComponentType<{
+    descriptors: { [key: string]: NavigationDescriptor },
+    navigation: NavigationScreenProp<S>,
+  }>;
+
+  declare export function createNavigator<O: *, S: *, NavigatorConfig: *>(
+    view: NavigationView<O, S>,
     router: NavigationRouter<S, O>,
-    routeConfigs?: NavigationRouteConfigMap,
     navigatorConfig?: NavigatorConfig
-  ): _NavigatorCreator<NavigationViewProps, S, O>;
+  ): NavigationContainer<NavigationStateRoute, *, *>;
 
   declare export function StackNavigator(
     routeConfigMap: NavigationRouteConfigMap,
--- src/views/StackView/StackViewLayout.js
+++ src/views/StackView/StackViewLayout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 import clamp from 'clamp';
 import {
@@ -11,14 +11,13 @@ import {
   Easing,
 } from 'react-native';
 
-import Card from './Card';
+import Card from './StackViewCard';
 import Header from '../Header/Header';
 import NavigationActions from '../../NavigationActions';
 import addNavigationHelpers from '../../addNavigationHelpers';
-import getChildEventSubscriber from '../../getChildEventSubscriber';
 import SceneView from '../SceneView';
 
-import TransitionConfigs from './TransitionConfigs';
+import TransitionConfigs from './StackViewTransitionConfigs';
 
 const emptyFunction = () => {};
 
@@ -58,7 +57,7 @@ const animatedSubscribeValue = animatedValue => {
   }
 };
 
-class CardStack extends React.Component {
+class StackViewLayout extends React.Component {
   /**
    * Used to identify the starting point of the position when the gesture starts, such that it can
    * be updated according to its relative position. This means that a card can effectively be
@@ -79,65 +78,29 @@ class CardStack extends React.Component {
    */
   _immediateIndex = null;
 
-  _screenDetails = {};
-
-  componentWillReceiveProps(props) {
-    if (props.screenProps !== this.props.screenProps) {
-      this._screenDetails = {};
-    }
-    props.scenes.forEach(newScene => {
-      if (
-        this._screenDetails[newScene.key] &&
-        this._screenDetails[newScene.key].state !== newScene.route
-      ) {
-        this._screenDetails[newScene.key] = null;
-      }
-    });
-  }
-
-  _getScreenDetails = scene => {
-    const { screenProps, navigation, router } = this.props;
-    let screenDetails = this._screenDetails[scene.key];
-    if (!screenDetails || screenDetails.state !== scene.route) {
-      const screenNavigation = addNavigationHelpers({
-        dispatch: navigation.dispatch,
-        state: scene.route,
-        addListener: getChildEventSubscriber(
-          navigation.addListener,
-          scene.route.key
-        ),
-      });
-      screenDetails = {
-        state: scene.route,
-        navigation: screenNavigation,
-        options: router.getScreenOptions(screenNavigation, screenProps),
-      };
-      this._screenDetails[scene.key] = screenDetails;
-    }
-    return screenDetails;
-  };
-
   _renderHeader(scene, headerMode) {
-    const { header } = this._getScreenDetails(scene).options;
+    const { options } = scene.descriptor;
+    const { header } = options;
 
     if (typeof header !== 'undefined' && typeof header !== 'function') {
       return header;
     }
 
-    const renderHeader = header || (props => <Header {...props} />);
+    const renderHeader = header || ((props: *) => <Header {...props} />);
     const {
       headerLeftInterpolator,
       headerTitleInterpolator,
       headerRightInterpolator,
     } = this._getTransitionConfig();
 
+    // We need to explicitly exclude `mode` since Flow doesn't see
+    // mode: headerMode override below and reports prop mismatch
     const { mode, ...passProps } = this.props;
 
     return renderHeader({
       ...passProps,
       scene,
       mode: headerMode,
-      getScreenDetails: this._getScreenDetails,
       leftInterpolator: headerLeftInterpolator,
       titleInterpolator: headerTitleInterpolator,
       rightInterpolator: headerRightInterpolator,
@@ -158,7 +121,7 @@ class CardStack extends React.Component {
     animatedSubscribeValue(props.position);
   }
 
-  _reset(resetToIndex, duration) {
+  _reset(resetToIndex, duration): void {
     Animated.timing(this.props.position, {
       toValue: resetToIndex,
       duration,
@@ -200,10 +163,19 @@ class CardStack extends React.Component {
     if (headerMode === 'float') {
       floatingHeader = this._renderHeader(this.props.scene, headerMode);
     }
-    const { navigation, position, layout, scene, scenes, mode } = this.props;
+    const {
+      navigation,
+      position,
+      layout,
+      scene,
+      scenes,
+      mode,
+      descriptors,
+    } = this.props;
     const { index } = navigation.state;
     const isVertical = mode === 'modal';
-    const { options } = this._getScreenDetails(scene);
+    const { options } = scene.descriptor;
+
     const gestureDirectionInverted = options.gestureDirection === 'inverted';
 
     const gesturesEnabled =
@@ -219,7 +191,7 @@ class CardStack extends React.Component {
             this._reset(index, 0);
           },
           onPanResponderGrant: () => {
-            position.stopAnimation(value => {
+            position.stopAnimation((value: number) => {
               this._isResponding = true;
               this._gestureStartValue = value;
             });
@@ -243,9 +215,12 @@ class CardStack extends React.Component {
               ? axisLength - (currentDragPosition - currentDragDistance)
               : currentDragPosition - currentDragDistance;
             // Compare to the gesture distance relavant to card or modal
+
+            const { options } = scene.descriptor;
+
             const {
               gestureResponseDistance: userGestureResponseDistance = {},
-            } = this._getScreenDetails(scene).options;
+            } = options;
             const gestureResponseDistance = isVertical
               ? userGestureResponseDistance.vertical ||
                 GESTURE_RESPONSE_DISTANCE_VERTICAL
@@ -346,7 +321,7 @@ class CardStack extends React.Component {
     return (
       <View {...handlers} style={containerStyle}>
         <View style={styles.scenes}>
-          {scenes.map(s => this._renderCard(s))}
+          {scenes.map((s: *) => this._renderCard(s))}
         </View>
         {floatingHeader}
       </View>
@@ -363,8 +338,10 @@ class CardStack extends React.Component {
     return 'float';
   }
 
-  _renderInnerScene(SceneComponent, scene) {
-    const { navigation } = this._getScreenDetails(scene);
+  _renderInnerScene(scene) {
+    const { options, navigation, getComponent } = scene.descriptor;
+    const SceneComponent = getComponent();
+
     const { screenProps } = this.props;
     const headerMode = this._getHeaderMode();
     if (headerMode === 'screen') {
@@ -383,7 +360,7 @@ class CardStack extends React.Component {
     }
     return (
       <SceneView
-        screenProps={this.props.screenProps}
+        screenProps={screenProps}
         navigation={navigation}
         component={SceneComponent}
       />
@@ -406,10 +383,6 @@ class CardStack extends React.Component {
     const style =
       screenInterpolator && screenInterpolator({ ...this.props, scene });
 
-    const SceneComponent = this.props.router.getComponentForRouteName(
-      scene.route.routeName
-    );
-
     return (
       <Card
         {...this.props}
@@ -417,7 +390,7 @@ class CardStack extends React.Component {
         style={[style, this.props.cardStyle]}
         scene={scene}
       >
-        {this._renderInnerScene(SceneComponent, scene)}
+        {this._renderInnerScene(scene)}
       </Card>
     );
   };
@@ -437,4 +410,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CardStack;
+export default StackViewLayout;
